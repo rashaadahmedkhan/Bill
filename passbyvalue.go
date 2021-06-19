7229c91abbac3a7d4545b810d8cb647dd07b8a75
Rashaad Ahmed Khan <rashaadakhan@gmail.com>
2021-06-19 20:41:45 +0530

    passbyvalue

---
+++ passbyvalue.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+)
+
+func updateName(x string) string{
+    x = "rash"
+    return x
+}
+
+func updateMenu(y map[string]int) {
+    y["coffee"] = 100
+}
+
+//type1 (non-pointer values): string, int, bool, float, array, struct
+//type2 (pointer wrapper values): slice, map, function
+func main() {
+    name := "rashaad"
+    name = updateName(name)
+    fmt.Println(name)
+
+    menu := map[string] int {
+        "oreo": 10,
+        "milk": 20,
+    }
+    updateMenu(menu)
+    fmt.Println(menu)
+}
