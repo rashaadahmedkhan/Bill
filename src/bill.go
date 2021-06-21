be124c1eb86905d928efeee9f3db211fe4a20840
Rashaad Ahmed Khan <rashaadakhan@gmail.com>
2021-06-22 00:33:35 +0530

    added user input

--- src/bill.go
+++ src/bill.go
@@ -19,18 +19,31 @@ func newBill(name string)bill {
 }
 
 //to format the bill
-func (b bill) format() string {
+func (b *bill) format() string {
     fs := "Bill breakdown: \n"
     var total float64 = 0
 
     //list items
     for k, v := range b.items {
-        fs += fmt.Sprintf("%-25v Rs%v \n", k+":", v)
+        fs += fmt.Sprintf("%-25v Rs %v \n", k+":", v)
         total += v
     }
 
+    //tip
+    fs += fmt.Sprintf("%-25v Rs %0.2f", "Tip:", b.tip,)
+
     //total
-    fs += fmt.Sprintf("%-25v Rs%0.2f", "Total:", total)
+    fs += fmt.Sprintf("\n%-25v Rs %0.2f", "Total:", total+b.tip)
 
     return fs
+}
+
+//to update tip
+func (b *bill) updateTip(tip float64) {
+    b.tip = tip
+}
+
+//add items to the bill
+func (b *bill) addItem(name string, price float64) {
+    b.items[name] = price
 }
--- src/main.go
+++ src/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+    fmt.Print(prompt)
+    input, err := r.ReadString('\n')
+
+    return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+    reader := bufio.NewReader(os.Stdin)
+    name, _ := getInput("Create a new bill name: ", reader)
+
+    b := newBill(name)
+    fmt.Println("Created Bill for", b.name)
+
+    return b
+}
+
+func promptOptions(b bill) {
+    reader := bufio.NewReader(os.Stdin)
+    opt, _ := getInput("Choose option (a: add item, s: save bill, t: update tip): ", reader)
+    fmt.Println(opt)
+}
 
 func main() {
-    mybill := newBill("Saleem bhai's bill")
-    fmt.Println(mybill.format())
+    mybill := createBill()
+    promptOptions(mybill)
 }
